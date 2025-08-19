aacc1be9df77a8c35f85456ad1401a03ee41e051
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2025-08-19 18:26:35 -0400

    Load dotenv and get api key + create gemini instance

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenvi
+from google import genai
+
 def main():
-    print("Hello from agent!")
+    # Loading variables from .env,
+    load_dotenvi()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    # Creating new instance of Gemini client
+    client = genai.Client(api_key=api_key)
+    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    # Getting response from client
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=content
+    )
+
+    print(response.text)
 
 
 if __name__ == "__main__":
