018ebf9db4e106e7a2d5ff4b4aa2ae94a6281202
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2025-08-19 22:41:25 -0400

    Add test function calls

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -11,8 +11,18 @@ def get_files_info(working_directory, directory="."):
         return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
     elif not os.path.isdir(target_abs_path):
         return f"Error: \"{directory}\" is not a directory"
+    
+    try:
+        files_info = directory_contents(full_path)
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-    return directory_contents(full_path)
+    if directory == ".":
+        return f"Result for current directory:\n{files_info}"
+    else:
+        return f"Result for '{directory}' directory:\n{files_info}"
+
+
 
 
 
@@ -23,5 +33,13 @@ def get_files_info(working_directory, directory="."):
 
 def directory_contents(directory_path):
     content_container = os.listdir(directory_path)
-    print(content_container) 
+    
+    # Loop through formatting string
+    for i in range(len(content_container)):
+        item_name = content_container[i]
+        item_path = os.path.join(directory_path, item_name)
+        item_size = os.path.getsize(item_path)
+        is_dir = os.path.isdir(item_path)
+        content_container[i] = f"- {item_name}: file_size={item_size} bytes, is_dir={is_dir}"
+    return "\n".join(content_container)
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     # Loading variables from .env,
@@ -9,18 +11,33 @@ def main():
 
     # Creating new instance of Gemini client
     client = genai.Client(api_key=api_key)
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+    # Checks for no conent provided
+    try:
+        user_prompt = sys.argv[1]
+    except IndexError:
+        sys.exit(1)
+    
+    if user_prompt == "":
+        sys.exit(1)
+
+    # Keep prompts as a list to play a role in the conversation
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
 
     # Getting response from client
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=content
+        contents=messages
     )
-
-    # Printing the prompt response and token usage 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n Response tokens: {reponse.usage_metadata.candidates_token_count}")
-
+    
+    # Handle verbose flag --verbose (give more info)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
